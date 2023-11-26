f56cd40091a98d335722ea1a2f9e94f935d63279
conscience osakue <consciencenosa@gmail.com>
2023-11-25 18:01:28 -0600

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        /*This is the constructor which is a special method
+        that gets invoked when instantiating a new instance of class.*/
+        public Car()
+        {
+ 
+        }
+        //Create a Make property of type string that is public
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Classes
+﻿using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // These are the properties 
+            var myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2008;
+
+            Console.WriteLine("Toyota, Camry, 2008.");
         }
+        
     }
 }
